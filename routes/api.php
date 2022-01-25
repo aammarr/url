ab78caf5c7c31229b631e3c9ada6639de89d8e1b
Ammar Ahmed <aammarr.ahmed@gmail.com>
2022-01-25 22:21:46 +0500

    passport

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('login', 'App\Http\Controllers\PassportController@login');
 Route::post('register', 'App\Http\Controllers\PassportController@register');
+Route::post('login', 'App\Http\Controllers\PassportController@login');
  
 Route::middleware('auth:api')->group(function () {
     Route::get('user', 'App\Http\Controllers\PassportController@details');
